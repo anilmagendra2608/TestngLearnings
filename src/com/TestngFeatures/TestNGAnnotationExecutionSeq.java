61414aa7dac519e7f17a634b33324c4b4540ce35
Anil Magendra <anil.magene@gmail.com>
2021-03-03 20:46:04 +1000

    refactored packages

---
+++ src/com/TestngFeatures/TestNGAnnotationExecutionSeq.java
@@ -0,0 +1,52 @@
+package com.TestngFeatures;
+
+import org.testng.annotations.*;
+
+public class TestNGAnnotationExecutionSeq {
+
+    // Sequence of Annotation execution listed below
+    @BeforeSuite
+    public void testBeforeSuite() {
+        System.out.println("@BeforeSuite Called");
+    }
+
+    @BeforeTest
+    public void testBeforeTest() {
+        System.out.println("@BeforeTest Called");
+    }
+
+    @BeforeClass
+    public void testBeforeClass() {
+        System.out.println("@BeforeClass Called");
+    }
+
+    @BeforeMethod
+    public void testBeforeMethod() {
+        System.out.println("@BeforeMethod Called");
+    }
+
+    @Test
+    public void testTest() {
+        System.out.println("@Test Called");
+    }
+
+    @AfterMethod
+    public void testAfterMethod() {
+        System.out.println("@AfterMethod Called");
+    }
+
+    @AfterClass
+    public void testAfterClass(){
+        System.out.println("@AfterClass Called");
+    }
+
+    @AfterTest
+    public void testAfterTest(){
+        System.out.println("@AfterTest Called");
+    }
+
+    @AfterSuite
+    public void testAfterSuite(){
+        System.out.println("@AfterSuite Called");
+    }
+}
